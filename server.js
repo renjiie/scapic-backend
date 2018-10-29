2d3ffbc5896b75ed2babe0cdfcda957916d799f9
Renjiie <you@example.com>
2018-10-29 21:47:21 +0530

    make it better

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ app.use(cors());
 const db = knex({
   client: 'pg', 
   connection: {
-  	connectionString : process.env.DATABASE_URL,
-    ssl : true
+  	host:'127.0.0.1',
+  	user:'Renjiie',
+  	password : 'rennie!95',
+  	database:'smart_brain'
   }
 });
 
